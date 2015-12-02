531f81d4792a1e2827d9f4f75caeab2ea422924e
Nick Pettazzoni <pettazz@gmail.com>
2015-12-02 12:13:26 -0500

    coming soon page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>Nick and Wendy - August 27th, 2016</title>
+        <meta name="viewport" content="width=400, height=device-height">
+        <meta name="description" content="Nick and Wendy, August 27th, 2016">
+
+        <!-- Le styles -->
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic|Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link href="/assets/css/main.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <div class="site-wrapper">
+          <div class="site-wrapper-inner">
+            <div class="cover-container">
+
+              <div class="inner cover">
+                <h1 class="cover-heading">Nick and Wendy, August 27th, 2016</h1>
+                <p class="lead">Details are coming soon</p>             
+              </div>
+
+              <div class="mastfoot">
+                <div class="inner">
+                    <p>
+                        <a href="https://github.com/pettazz/nwrau" target="_blank"><small>lolrau</small></a>
+                    </p>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+    </body>
+</html>
