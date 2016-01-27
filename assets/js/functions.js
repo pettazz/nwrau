4611e7c7044ce5398dca1be014faf6bd63715f47
Nick Pettazzoni <pettazz@gmail.com>
2016-01-27 00:50:58 -0500

    fix issues with jquery plugins from lib

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,25 +1,3 @@
-/**
- * @author Alexander Manzyuk <admsev@gmail.com>
- * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
- * https://github.com/admsev/jquery-play-sound
- * Usage: $.playSound('http://example.org/sound.mp3');
-**/
-
-(function($){
-
-  $.extend({
-    playSound: function(){
-      return $(
-        '<audio autoplay="autoplay" style="display:none;">'
-          + '<source src="' + arguments[0] + '" />'
-          + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false" class="playSound" />'
-        + '</audio>'
-      ).appendTo('body');
-    }
-  });
-
-})(jQuery);
-
 function scrollPercentage() {
     var winTop = $(window).scrollTop(),
         docHeight = $(document).height(),
@@ -55,6 +33,10 @@ $(document).ready(function(){
         });
     });
 
+    var eggloader2 = new Konami(function(){
+        $.onTheRitz();
+    }, '8085847378');
+
     if(window.location.href.indexOf('the-proposal') > -1){
         var image = $('img[src="/assets/images/proposal1.JPG"]');
 
@@ -80,3 +62,128 @@ $(document).ready(function(){
         });
     }
 });
+
+/********************************************************************************
+                            jquery plugin land
+*********************************************************************************/
+
+/**
+ * @author Alexander Manzyuk <admsev@gmail.com>
+ * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
+ * https://github.com/admsev/jquery-play-sound
+ * Usage: $.playSound('http://example.org/sound.mp3');
+**/
+
+(function($){
+
+  $.extend({
+    playSound: function(){
+      return $(
+        '<audio autoplay="autoplay" style="display:none;" class="playSound">'
+          + '<source src="' + arguments[0] + '" />'
+          + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false" />'
+        + '</audio>'
+      ).appendTo('body');
+    },
+
+    stopSound: function(){
+        return $('body > audio.playSound').remove();
+    }
+  });
+
+})(jQuery);
+
+/**
+ * Easings I stole from jquery-ui
+**/
+
+(function() {
+    // based on easing equations from Robert Penner (http://www.robertpenner.com/easing)
+
+    var baseEasings = {};
+
+    $.each( [ "Quad", "Cubic", "Quart", "Quint", "Expo" ], function( i, name ) {
+        baseEasings[ name ] = function( p ) {
+            return Math.pow( p, i + 2 );
+        };
+    });
+
+    $.extend( baseEasings, {
+        Sine: function( p ) {
+            return 1 - Math.cos( p * Math.PI / 2 );
+        },
+        Circ: function( p ) {
+            return 1 - Math.sqrt( 1 - p * p );
+        },
+        Elastic: function( p ) {
+            return p === 0 || p === 1 ? p :
+                -Math.pow( 2, 8 * (p - 1) ) * Math.sin( ( (p - 1) * 80 - 7.5 ) * Math.PI / 15 );
+        },
+        Back: function( p ) {
+            return p * p * ( 3 * p - 2 );
+        },
+        Bounce: function( p ) {
+            var pow2,
+                bounce = 4;
+
+            while ( p < ( ( pow2 = Math.pow( 2, --bounce ) ) - 1 ) / 11 ) {}
+            return 1 / Math.pow( 4, 3 - bounce ) - 7.5625 * Math.pow( ( pow2 * 3 - 2 ) / 22 - p, 2 );
+        }
+    });
+
+    $.each( baseEasings, function( name, easeIn ) {
+        $.easing[ "easeIn" + name ] = easeIn;
+        $.easing[ "easeOut" + name ] = function( p ) {
+            return 1 - easeIn( 1 - p );
+        };
+        $.easing[ "easeInOut" + name ] = function( p ) {
+            return p < 0.5 ?
+                easeIn( p * 2 ) / 2 :
+                1 - easeIn( p * -2 + 2 ) / 2;
+        };
+    });
+})();
+
+/**
+ * @author Nick Pettazzoni <@pettazz>
+ * Released under MIT License
+ * Usage: $.onTheRitz();
+**/
+
+(function($){
+
+  $.extend({
+    onTheRitz: function(){
+        var isPutinOnTheLoose = $('#big-bad-vlad').length > 0;
+        if(!isPutinOnTheLoose){
+            var viewportWidth = $(window).width();
+            var viewportHeight = $(window).height();
+            var $vlad = $('<img src="/assets/images/nobleviewresident.png" id="big-bad-vlad" style="display:none;" />');
+
+            $.playSound('/assets/anthem.mp3');
+            $('#sb-site').fadeOut('fast', function(){
+                $('body').append($vlad);
+                $vlad.css({
+                    position: 'absolute',
+                    width: 60
+                });
+                $vlad.offset({
+                    top: $(window).scrollTop() + viewportHeight / 4, 
+                    left: viewportWidth + 100 
+                });
+                $vlad.show();
+
+                $vlad.animate({ 
+                    left: -600,
+                    width: 600
+                }, 10000, 'easeInQuart', function(){
+                    $.stopSound();
+                    $vlad.remove();
+                    $('#sb-site').fadeIn('fast');
+                });
+            });
+        }
+    }
+  });
+
+})(jQuery);
--- assets/js/libs/konami.js
+++ assets/js/libs/konami.js
@@ -1,6 +1,6 @@
 /*
- * Konami-JS ~ 
- * :: Now with support for touch events and multiple instances for 
+ * Konami-JS ~
+ * :: Now with support for touch events and multiple instances for
  * :: those situations that call for multiple easter eggs!
  * Code: http://konami-js.googlecode.com/
  * Examples: http://www.snaptortoise.com/konami-js
@@ -10,7 +10,7 @@
  * Tested in: Safari 4+, Google Chrome 4+, Firefox 3+, IE7+, Mobile Safari 2.2.1 and Dolphin Browser
  */
 
-var Konami = function (callback) {
+var Konami = function (callback, pattern) {
     var konami = {
         addEvent: function (obj, type, fn, ref_obj) {
             if (obj.addEventListener)
@@ -20,12 +20,12 @@ var Konami = function (callback) {
                 obj["e" + type + fn] = fn;
                 obj[type + fn] = function () {
                     obj["e" + type + fn](window.event, ref_obj);
-                }
+                };
                 obj.attachEvent("on" + type, obj[type + fn]);
             }
         },
         input: "",
-        pattern: "38384040373937396665",
+        pattern: pattern || "38384040373937396665",
         load: function (link) {
             this.addEvent(document, "keydown", function (e, ref_obj) {
                 if (ref_obj) konami = ref_obj; // IE
@@ -49,13 +49,18 @@ var Konami = function (callback) {
             start_y: 0,
             stop_x: 0,
             stop_y: 0,
-            tap: false,
+            tapTolerance: 8,
             capture: false,
             orig_keys: "",
             keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
             code: function (link) {
                 konami.code(link);
             },
+      touchCapture: function(evt) {
+                konami.iphone.start_x = evt.changedTouches[0].pageX;
+                konami.iphone.start_y = evt.changedTouches[0].pageY;
+                konami.iphone.capture = true;        
+      },
             load: function (link) {
                 this.orig_keys = this.keys;
                 konami.addEvent(document, "touchmove", function (e) {
@@ -63,30 +68,36 @@ var Konami = function (callback) {
                         var touch = e.touches[0];
                         konami.iphone.stop_x = touch.pageX;
                         konami.iphone.stop_y = touch.pageY;
-                        konami.iphone.tap = false;
-                        konami.iphone.capture = false;
                         konami.iphone.check_direction();
                     }
                 });
                 konami.addEvent(document, "touchend", function (evt) {
-                    if (konami.iphone.tap == true) konami.iphone.check_direction(link);
+                    konami.touchCapture(evt);
+                    konami.iphone.check_direction(link);
                 }, false);
                 konami.addEvent(document, "touchstart", function (evt) {
-                    konami.iphone.start_x = evt.changedTouches[0].pageX;
-                    konami.iphone.start_y = evt.changedTouches[0].pageY;
-                    konami.iphone.tap = true;
-                    konami.iphone.capture = true;
+                    konami.touchCapture(evt);
                 });
             },
             check_direction: function (link) {
-                x_magnitude = Math.abs(this.start_x - this.stop_x);
-                y_magnitude = Math.abs(this.start_y - this.stop_y);
-                x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
-                y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
-                result = (x_magnitude > y_magnitude) ? x : y;
-                result = (this.tap == true) ? "TAP" : result;
-
-                if (result == this.keys[0]) this.keys = this.keys.slice(1, this.keys.length);
+                var x_magnitude = Math.abs(this.start_x - this.stop_x);
+                var y_magnitude = Math.abs(this.start_y - this.stop_y);
+                var hasMoved = (x_magnitude > this.tapTolerance || y_magnitude > this.tapTolerance);
+                var result;
+                if (this.capture === true && hasMoved) {
+                    this.capture = false;
+                    var x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
+                    var y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
+                    var result = (x_magnitude > y_magnitude) ? x : y;
+                }
+                else if (this.capture === false && !hasMoved) {
+                    result = (this.tap == true) ? "TAP" : result;
+                    result = "TAP";
+                }
+                if (result) {
+                    if (result == this.keys[0]) this.keys = this.keys.slice(1, this.keys.length);
+                    else this.keys = this.orig_keys;
+                }
                 if (this.keys.length == 0) {
                     this.keys = this.orig_keys;
                     this.code(link);
